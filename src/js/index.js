12b525cf6873c6cffe927eaf1f205e3701c9a9ac
Tomasz Paterka <tomekpaterka@gmail.com>
2019-11-21 21:42:22 +0100

    add app controller, add serachView functions, add base file with DOM elements and loader functions

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,42 @@ import '../css/style.css';
 import '../img/favicon.png';
 import '../img/icons.svg';
 import '../img/logo.png';
-import '../img/test-1.jpg';
+// import '../img/test-1.jpg';
 
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
-const search = new Search('pizza');
-console.log(search);
+// Global state of the app
+// - Search object
+// - Recipe object
+// - Shopping list object
+// - Liked object
 
-search.getResults();
+const state = {};
+
+const controlSearch = async () => {
+  const query = searchView.getInput();
+  // console.log(query);
+  if (query) {
+    state.search = new Search(query);
+
+    searchView.cleanInput();
+    searchView.clearResults();
+
+    renderLoader(elements.searchRes);
+
+    await state.search.getResults();
+    console.log(state.search.result);
+
+    clearLoader();
+
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
