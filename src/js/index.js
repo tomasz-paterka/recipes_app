bc8fd6d137c5d5cebc4c239734d2c951443b94b5
Tomasz Paterka <tomekpaterka@gmail.com>
2019-11-24 16:56:04 +0100

    add List model and listView for shopping list

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import '../img/logo.png';
 
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -98,3 +99,5 @@ elements.recipe.addEventListener('click', e => {
     recipeView.updateServingsIngredients(state.recipe);
   } 
 });
+
+window.l = new List();
